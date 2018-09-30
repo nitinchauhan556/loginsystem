1bd233c7ab761bd31e29d87f19d101e684e0e400
Nitin Chauhan <43624052+nitinchauhan556@users.noreply.github.com>
2018-09-30 14:21:39 +0530

    header
    Header of a page

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link type="text/css" rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+      <nav>
+          <div class="main-wrapper">
+            <ul>
+             <li><a href="index.php" >Home </a></li>
+            </ul>
+            <div class="nav-login" >
+               <form>
+                  <input type="text" name="uid" placeholder="username/email">
+                  <input type="password" name="pwd" placehoder="password">
+                  <button name="submit" type="submit">login</button>
+               </form> 
+              <a href="signup.php">Sign up</a>     
+            </div>
+        </div>
+      </nav>
+    </header>
